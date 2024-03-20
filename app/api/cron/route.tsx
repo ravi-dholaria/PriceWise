dc603925d1b02ce7c74ddb01be0e866e833f9e7d
ravi-dholariya <200540107103@darshan.ac.in>
2024-03-20 11:21:44 +0530

    Done

--- app/api/cron/route.tsx
+++ app/api/cron/route.tsx
@@ -1,4 +1,6 @@
-import { NextApiResponse, NextApiRequest } from "next";
+"use client";
+import { NextResponse } from "next/server";
+
 import {
   getLowestPrice,
   getHighestPrice,
@@ -10,10 +12,12 @@ import Product from "@/lib/models/product.model";
 import { scrapeAmazonProduct } from "@/lib/scraper";
 import { generateEmailBody, sendEmail } from "@/lib/nodemailer";
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
+export const maxDuration = 10; // This function can run for a maximum of 300 seconds
+export const dynamic = "force-dynamic";
+export const revalidate = 0;
+export const runtime = "edge";
+
+export async function GET(request: Request) {
   try {
     connectToDatabase();
 
@@ -21,15 +25,19 @@ export default async function handler(
 
     if (!products) throw new Error("No product fetched");
 
+    // ======================== 1 SCRAPE LATEST PRODUCT DETAILS & UPDATE DB
     const updatedProducts = await Promise.all(
       products.map(async (currentProduct) => {
+        // Scrape product
         const scrapedProduct = await scrapeAmazonProduct(currentProduct.url);
 
         if (!scrapedProduct) return;
 
         const updatedPriceHistory = [
           ...currentProduct.priceHistory,
-          { price: scrapedProduct.currentPrice },
+          {
+            price: scrapedProduct.currentPrice,
+          },
         ];
 
         const product = {
@@ -40,11 +48,15 @@ export default async function handler(
           averagePrice: getAveragePrice(updatedPriceHistory),
         };
 
+        // Update Products in DB
         const updatedProduct = await Product.findOneAndUpdate(
-          { url: product.url },
+          {
+            url: product.url,
+          },
           product
         );
 
+        // ======================== 2 CHECK EACH PRODUCT'S STATUS & SEND EMAIL ACCORDINGLY
         const emailNotifType = getEmailNotifType(
           scrapedProduct,
           currentProduct
@@ -55,13 +67,16 @@ export default async function handler(
             title: updatedProduct.title,
             url: updatedProduct.url,
           };
+          // Construct emailContent
           const emailContent = await generateEmailBody(
             productInfo,
             emailNotifType
           );
+          // Get array of user emails
           const userEmails = updatedProduct.users.map(
             (user: any) => user.email
           );
+          // Send email notification
           await sendEmail(emailContent, userEmails);
         }
 
@@ -69,9 +84,11 @@ export default async function handler(
       })
     );
 
-    res.status(200).json({ message: "Ok", data: updatedProducts });
+    return NextResponse.json({
+      message: "Ok",
+      data: updatedProducts,
+    });
   } catch (error: any) {
-    console.error(`Failed to get all products: ${error.message}`);
-    res.status(500).json({ error: "Internal Server Error" });
+    throw new Error(`Failed to get all products: ${error.message}`);
   }
 }
